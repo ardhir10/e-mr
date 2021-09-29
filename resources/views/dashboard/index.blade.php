b3459fd052db500e47cae54d96bff3e03e435c2d
Ardhi Ramadhan <ardhiramadhan98@gmail.com>
2021-09-30 03:38:53 +0700

    Chart

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -24,37 +24,35 @@
         <!-- end page title -->
 
         <div class="row">
-            <div class="col-xl-3 col-sm-6">
+            <div class="col-xl-4 col-sm-6">
                 <!-- Card -->
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between">
                             <div>
                                 <h6 class="font-size-xs text-uppercase">Total Rekam Medis</h6>
-                                <h4 class="mt-4 font-weight-bold mb-2 d-flex align-items-center" style="    font-size: 46px;
-    font-weight: 500;
-    color: #A1CB47;">
+                                <h5 class="mt-2 font-weight-bold mb-2 d-flex align-items-center"
+                                    style="    font-size: 40px;font-weight: 500;color: #A1CB47;">
                                     {{number_format($total_rekam_medis->jrm,0,',','.')}}
-                                </h4>
+                                </h5>
                                 <a href="{{route('rekam-medis.index')}}" class="text-muted">Lihat Detail </a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-sm-6">
+            <div class="col-xl-4 col-sm-6">
                 <!-- Card -->
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between">
                             <div>
                                 <h6 class="font-size-xs text-uppercase">Total Rawat Jalan</h6>
-                                <h4 class="mt-4 font-weight-bold mb-2 d-flex align-items-center" style="    font-size: 46px;
-    font-weight: 500;
-    color: #1965AA;">
+                                <h5 class="mt-2 font-weight-bold mb-2 d-flex align-items-center"
+                                    style="    font-size: 40px;font-weight: 500;color: #1965AA;">
                                     {{number_format(count($total_rawat_jalan),0,',','.')}}
 
-                                </h4>
+                                </h5>
                                 <a href="{{route('rawat-jalan.index')}}" class="text-muted">Lihat Detail </a>
                             </div>
 
@@ -62,18 +60,17 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-sm-6">
+            <div class="col-xl-4 col-sm-6">
                 <!-- Card -->
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between">
                             <div>
                                 <h6 class="font-size-xs text-uppercase">Total Rawat Inap</h6>
-                                 <h4 class="mt-4 font-weight-bold mb-2 d-flex align-items-center" style="    font-size: 46px;
-    font-weight: 500;
-    color: #E76D19;">
+                                <h5 class="mt-2 font-weight-bold mb-2 d-flex align-items-center"
+                                    style="    font-size: 40px;font-weight: 500;color: #E76D19;">
                                     {{number_format(count($total_rawat_inap),0,',','.')}}
-                                </h4>
+                                </h5>
                                 <a href="{{route('rawat-inap.index')}}" class="text-muted">Lihat Detail </a>
                             </div>
 
@@ -85,6 +82,29 @@
 
 
         </div> <!-- end row-->
+        <div class="row">
+            <div class="col-xl-12 col-sm-12">
+                <!-- Card -->
+                <div class="card">
+                    <div class="card-header">
+                        <h6 class="font-size-xs card-title text-uppercase">Last Year Statistic</h6>
+
+                    </div>
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between">
+                            <div id=""
+                                style="padding:20px;height: 450px;width:100%;background:#100C2A;border-radius:30px">
+                                <div id="dashboard-statistic" style="height: 100%"></div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
 
 
 
@@ -92,3 +112,103 @@
     <!-- container-fluid -->
 </div>
 @endsection
+
+@push('scripts')
+<link rel="stylesheet" href="">
+<script src="{{asset('/assets')}}/libs/echart/echarts.min.js"></script>
+
+
+<script>
+    generateChart();
+
+    function generateChart() {
+
+        var chartDom = document.getElementById('dashboard-statistic');
+        var myChart = echarts.init(chartDom, 'dark');
+        var option;
+
+        option = {
+            legend: {},
+            xAxis: [
+                 {
+                    type: 'category',
+                    data: @json($chart['ts']['rm']),
+                    position:'bottom',
+                },
+                {
+                    type: 'category',
+                    data: @json($chart['ts']['rj']),
+                    position:'bottom',
+                     offset: 20
+
+                },
+                {
+                    type: 'category',
+                    data: @json($chart['ts']['ri']),
+                    position:'bottom',
+                     offset: 40
+
+                },
+               
+            ],
+            yAxis: {
+                type: 'value'
+            },
+            grid: {
+                top: "10%",
+                left: "8%",
+                bottom: "20%",
+                right: "8%",
+            },
+            dataZoom :{
+                type:"inside"
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            series: [
+                 {
+                    name: "Rekam Medis",
+                    data: @json($chart['series']['rm']),
+                    type: 'line',
+                    symbol:'rect',
+                     symbolSize: 6,
+
+                    xAxisIndex:0,
+                    itemStyle: {
+                        color: '#A1CB47'
+                    }
+                },
+                {
+                    name: "Rawat Jalan",
+                    data: @json($chart['series']['rj']),
+                    type: 'line',
+                    symbol:'circle',
+                     symbolSize: 6,
+
+                    xAxisIndex:1,
+                    itemStyle: {
+                        color: '#1965AA'
+                    }
+                },
+                {
+                    name: "Rawat Inap",
+                    data: @json($chart['series']['ri']),
+                    type: 'line',
+                    symbol:'circle',
+                     symbolSize: 6,
+                    xAxisIndex:2,
+                    itemStyle: {
+                        color: '#E76D19'
+                    }
+                },
+               
+            ]
+        };
+
+        option && myChart.setOption(option);
+
+    }
+
+</script>
+@endpush
